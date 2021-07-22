25636b0e95c3e658b5534a8774789ae027efe9f0
one-four104 <susannaxchan@gmail.com>
2021-07-22 15:15:04 +0800

    Changes to prevent auto trigger of jobs

--- lambda/comprehend.py
+++ lambda/comprehend.py
@@ -33,13 +33,18 @@ def start_sentiment_analysis(comprehend, input_data_config, output_data_config, 
     return job_id
     
 def lambda_handler(event, context):
+    if "Filename" not in event: 
+        return {
+            'statusCode': 400,
+            'body': json.dumps('No file!')
+        }
+        
     comprehend = boto3.client(service_name='comprehend', region_name='ap-southeast-1')
     s3 = boto3.client('s3')
 
-    input_s3_url = "s3://hackathon-cleaned-data/Questions.csv"
+    input_s3_url = "s3://hackathon-cleaned-data/" + event["Filename"]
     input_doc_format = "ONE_DOC_PER_LINE"
     output_s3_url = "s3://hackathon-comprehend-raw-results"
-    data_access_role_arn = "arn:aws:iam::322895421085:role/comprehend-s3-access"  # TODO check role permission
     number_of_topics = 10
 
     input_data_config = {"S3Uri": input_s3_url, "InputFormat": input_doc_format}
@@ -48,17 +53,19 @@ def lambda_handler(event, context):
     topic_job_name = "Topic Modelling Job"
     sentiment_job_name = "Sentiment Analysis Job"
     
-    topic_job_id = start_topic_modelling(
-        comprehend, input_data_config, output_data_config, data_access_role_arn, number_of_topics, topic_job_name)
-    sentiment_job_id = start_sentiment_analysis(
-        comprehend, input_data_config, output_data_config, data_access_role_arn, number_of_topics, sentiment_job_name)
-
-    describe_topics_detection_job_result = comprehend.describe_topics_detection_job(JobId=topic_job_id)
-    describe_sentiment_detection_job_result = comprehend.describe_sentiment_detection_job(JobId=sentiment_job_id)
+    if event["Topic"]: 
+        topic_job_id = start_topic_modelling(
+            comprehend, input_data_config, output_data_config, number_of_topics, topic_job_name)
+        describe_topics_detection_job_result = comprehend.describe_topics_detection_job(JobId=topic_job_id)
+        print('describe_topics_detection_job_result: ', describe_topics_detection_job_result)
+        
+    if event["Sentiment"]:
+        sentiment_job_id = start_sentiment_analysis(
+            comprehend, input_data_config, output_data_config, number_of_topics, sentiment_job_name)
+        describe_sentiment_detection_job_result = comprehend.describe_sentiment_detection_job(JobId=sentiment_job_id)
+        print('describe_sentiment_detection_job_result: ', describe_sentiment_detection_job_result)
     
-    # while describe_topics_detection_job_result['TopicsDetectionJobProperties']['JobStatus'] != 'COMPLETED' and \
-    #     describe_topics_detection_job_result['TopicsDetectionJobProperties']['JobStatus'] != 'FAILED':
-    #     describe_topics_detection_job_result = comprehend.describe_topics_detection_job(JobId=job_id)
-     
-    print('describe_topics_detection_job_result: ', describe_topics_detection_job_result)
-    print('describe_sentiment_detection_job_result: ', describe_sentiment_detection_job_result)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Job started!')
+    }
