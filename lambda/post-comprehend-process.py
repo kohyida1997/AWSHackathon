eb28585f21eebbf5cbdc73585673458de6ccb2e4
one-four104 <susannaxchan@gmail.com>
2021-07-23 14:46:21 +0800

    Remove extra code and fix cleaned bucket configs

--- lambda/post-comprehend-process.py
+++ lambda/post-comprehend-process.py
@@ -42,7 +42,7 @@ def create_enriched_csv_with_text_timestamp_and_topics(text_timestamps, topics_o
     return temp_csv_with_docnames
 
 
-def unzip_topics_output(s3_client, src_bucket, gzipped_key, dest_bucket, dest_key, cleaned_bucket, text_timestamps_key):
+def process_topics_output(s3_client, src_bucket, gzipped_key, dest_bucket, dest_key, cleaned_bucket):
     input_tar_file = s3_client.get_object(Bucket=src_bucket, Key=gzipped_key)
     input_tar_content = input_tar_file['Body'].read()
 
@@ -56,6 +56,8 @@ def unzip_topics_output(s3_client, src_bucket, gzipped_key, dest_bucket, dest_ke
                 if tar_resource.name.find('doc-topics') > -1:
                     docname_prefix = get_docname_prefix_from_doc_topics_output(
                         topics_output)
+                    print(docname_prefix)
+                    text_timestamps_key = 'timestamp-' + docname_prefix
                     text_timestamps = pd.read_csv(BytesIO(s3_client.get_object(
                         Bucket=cleaned_bucket, Key=text_timestamps_key)['Body'].read()))
                     text_timestamps_docname = get_docname_added_csv(
@@ -119,32 +121,17 @@ def lambda_handler(event, context):
     job_id = get_job_id(gzipped_key)
     print(gzipped_key, job_id)
     processed_bucket = os.environ['DEST_BUCKET']
-    cleaned_bucket = '' # Change accordingly
+    cleaned_bucket = os.environ['CLEAN_BUCKET']
     processed_key_topics = "TOPICS-output-" + job_id
     processed_key_sentiment = "SENTIMENT-output-" + job_id
-    text_timestamps_key = '' # Change accordingly
     score_types = ["Mixed", "Negative", "Neutral", "Positive"]
-
-
-    # Change this accordingly
-    text_and_timestamps_original_input_file_path = 'text-timestamp-questions-only-title.csv' # Change this to read the text-timestamps doc from cleaned bucket
-    topic_modelling_doc_topics_output_file_path = processed_key_topics + '-doc-topics.csv'
-
-    enriched_csv_with_text_timestamps_and_topics = (
-        create_enriched_csv_with_text_timestamp_and_topics(
-            text_and_timestamps_original_input_file_path, topic_modelling_doc_topics_output_file_path))
-
-    enriched_csv_file_path = text_and_timestamps_original_input_file_path.replace(
-        '.csv', '-enriched.csv')
-
-    enriched_csv_with_text_timestamps_and_topics.to_csv(enriched_csv_file_path, index=False)
 
     # initialize s3 client
     s3_client = boto3.client('s3')
 
     if gzipped_key.find('TOPICS') > -1:
-        unzip_topics_output(s3_client, raw_bucket, gzipped_key,
-                            processed_bucket, processed_key_topics, cleaned_bucket, text_timestamps_key) 
+        process_topics_output(s3_client, raw_bucket, gzipped_key,
+                            processed_bucket, processed_key_topics, cleaned_bucket) 
     elif gzipped_key.find('SENTIMENT') > -1:
         process_sentiment_output(s3_client, raw_bucket, gzipped_key,
                                  score_types, processed_bucket, processed_key_sentiment)
